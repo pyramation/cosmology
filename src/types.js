41213ff3234a4effbf0691445a747d8683321061
njerschow <nj18@nyu.edu>
2022-01-24 23:02:35 -0500

    WIP rebalancing (todo: swap routing & RPC calls)

--- src/types.js
+++ src/types.js
@@ -1,19 +1,9 @@
+import { Token } from './model/Token';
 
 /**
- * 
- * 
- * 
- * @typedef {{
- * symbol: string;
- * osmosisID: string;
- * contractAddress: string;
- * }} Token
- * 
- * 
  * @typedef {{
  * inToken: Token
  * outToken: Token
- * amount: number
+ * inAmount: number
  * }} Swap
  */
-
