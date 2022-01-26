ece8a3e0081bb554988b98dba72c5b0f8d0d61b3
Dan Lynch <pyramation@gmail.com>
2022-01-26 12:26:28 -0800

    pkg

--- packages/autosmosis/src/my-test.js
+++ packages/autosmosis/src/my-test.js
@@ -1,83 +1,86 @@
-import { OsmosisClient } from '../src/clients/osmosis';
 import { coins, coin } from '@cosmjs/launchpad';
-import {
-  DeepPartial,
-  MsgDelegate
-} from 'cosmjs-types/cosmos/staking/v1beta1/tx';
-
-import { Secp256k1HdWallet, SigningCosmosClient } from '@cosmjs/launchpad';
-import { AminoTypes, SigningStargateClient, StargateClient } from '@cosmjs/stargate';
+import { Secp256k1HdWallet } from '@cosmjs/launchpad';
+import { AminoTypes, SigningStargateClient } from '@cosmjs/stargate';
 import { decodeTxRaw, Registry } from '@cosmjs/proto-signing';
-import { MsgSend } from 'cosmjs-types/cosmos/bank/v1beta1/tx';
 import { osmosis } from './proto/generated/codecimpl';
 import axios from 'axios';
-import { TxRaw } from "cosmjs-types/cosmos/tx/v1beta1/tx";
+import { TxRaw } from 'cosmjs-types/cosmos/tx/v1beta1/tx';
 
-const NET = process.env.local ? "LOCAL" : "NOT_LOCAL";
+const NET = process.env.local ? 'LOCAL' : 'TESTNET';
 
-// localnet
-// const mnemonic =
-//   'health nest provide snow total tissue intact loyal cargo must credit wrist';
+const mnemonic =
+  NET === 'LOCAL'
+    ? 'health nest provide snow total tissue intact loyal cargo must credit wrist'
+    : 'mammal wrestle hybrid cart choose flee transfer filter fly object swamp rookie';
 
-//testnet
-const mnemonic = NET === 'LOCAL' ? 'health nest provide snow total tissue intact loyal cargo must credit wrist' : 'mammal wrestle hybrid cart choose flee transfer filter fly object swamp rookie'
-export const go = async () => {
+const chainId = NET === 'LOCAL' ? 'localnet-1' : 'osmosis-testnet-0';
+
+// rpc
+const rpcEndpoint =
+  NET === 'LOCAL' ? 'http://10.0.0.15:26657' : 'http://143.244.147.126:26657';
+
+// lcd
+const lcdEndpoint =
+  NET === 'LOCAL' ? 'http://10.0.0.15:1317' : 'http://143.244.147.126:1317';
+
+export const main = async () => {
+  // wallet
   const wallet = await Secp256k1HdWallet.fromMnemonic(mnemonic, {
     prefix: 'osmo'
   });
+  // address
   const accounts = await wallet.getAccounts();
-  const [{ address }] = accounts
-  const rpcEndpoint = NET === 'LOCAL' ? 'http://10.0.0.15:26657' : 'http://143.244.147.126:26657'; // 'http://143.244.147.126:26657';
+  const [{ address }] = accounts;
+  // lcd for account info
+
+  const authAccountInfo = await axios.get(
+    `${lcdEndpoint}/auth/accounts/${address}`
+  );
+
+  console.log({ authAccountInfo: authAccountInfo.data.result.value });
+
+  // registry
   const registry = new Registry();
 
-
-  const lcdResponse = await axios.get(NET === 'LOCAL' ? `http://10.0.0.15:1317/auth/accounts/${address}` : `http://143.244.147.126:1317/auth/accounts/${address}`);//`http://143.244.147.126:1317/auth/accounts/${address}`);
-  console.log({ lcdResponse: lcdResponse.data.result.value })
-
+  // aminotypes
   const aminoTypes = new AminoTypes({
     additions: {
       '/osmosis.gamm.v1beta1.MsgSwapExactAmountIn': {
         aminoType: 'osmosis/gamm/swap-exact-amount-in',
         toAmino: ({ sender, routes, tokenIn, tokenOutMinAmount }) => {
-          console.log("toAmino:", { sender, routes, tokenIn, tokenOutMinAmount });
           return {
             sender,
-            routes: routes.map(r => {
+            routes: routes.map((r) => {
               return {
                 pool_id: r.poolId,
                 token_out_denom: r.tokenOutDenom
-              }
+              };
             }),
             token_in: tokenIn,
             token_out_min_amount: tokenOutMinAmount
-          }
+          };
         },
         fromAmino: ({ sender, routes, token_in, token_out_min_amount }) => {
-          console.log("fromAmino:", { sender, routes, token_in, token_out_min_amount });
           return {
             sender,
-            routes: routes.map(r => {
+            routes: routes.map((r) => {
               return {
                 poolId: r.pool_id,
                 tokenOutDenom: r.token_out_denom
-              }
+              };
             }),
             tokenIn: token_in,
             tokenOutMinAmount: token_out_min_amount
-          }
+          };
         }
       }
     }
   });
 
-  registry.register('/osmosis.gamm.v1beta1.MsgSwapExactAmountIn', osmosis.gamm.v1beta1.MsgSwapExactAmountIn);
-  // registry.register('/osmosis/gamm/swap-exact-amount-in', MsgSend);
-  // registry.register('osmosis/gamm/swap-exact-amount-in', MsgSend);
-  // registry.register('osmosis.gamm.swap-exact-amount-in', MsgSend);
-  // registry.register('/osmosis.gamm.swap-exact-amount-in', MsgSend);
-
-
-  // console.log("type",registry.lookupType('/osmosis.gamm.v1beta1.MsgSwapExactAmountIn').)
+  registry.register(
+    '/osmosis.gamm.v1beta1.MsgSwapExactAmountIn',
+    osmosis.gamm.v1beta1.MsgSwapExactAmountIn
+  );
 
   const client = await SigningStargateClient.connectWithSigner(
     rpcEndpoint,
@@ -89,6 +92,7 @@ export const go = async () => {
     amount: coins(0, 'uosmo'),
     gas: '250000'
   };
+
   const msg = {
     typeUrl: '/osmosis.gamm.v1beta1.MsgSwapExactAmountIn',
     value: {
@@ -108,63 +112,27 @@ export const go = async () => {
       tokenOutMinAmount: '733197'
     }
   };
-  // const signed = await client.sign(address, [msg], fee, 'mymemo');
-  // console.log(signed);
-
-  // const res = await client.signAndBroadcast(address, [msg], fee, "yo memo");
-
 
   const { accountNumber, sequence } = await client.getSequence(address);
 
+  console.log({ accountNumber, sequence });
+
   const txRaw = await client.sign(address, [msg], fee, 'mymemo', {
-    'accountNumber': accountNumber,
-    'sequence': sequence,
-    'chainId': NET === 'LOCAL' ? 'localnet-1' : 'osmosis-testnet-0'
+    accountNumber: accountNumber,
+    sequence: sequence,
+    chainId
   });
   const txBytes = TxRaw.encode(txRaw).finish();
-  
-  const decoded = {
-    authInfo:decodeTxRaw(txBytes).authInfo,
-    body:decodeTxRaw(txBytes).body
-  }
-  console.log(JSON.stringify(decoded, null, 2));
 
-  // console.log({ txRawSig: txRaw.signatures, txBytes })
+  const decoded = {
+    authInfo: decodeTxRaw(txBytes).authInfo,
+    body: decodeTxRaw(txBytes).body
+  };
+  //   console.log(JSON.stringify(decoded, null, 2));
+
   const res = await client.broadcastTx(txBytes, 100000, 1000);
 
-
   console.log(res);
-
-  // await client.broadcastTx(signed.signatures)
-  // maybe we need to update registry
-
-  //   const client = new OsmosisClient({
-  //     wallet,
-  //     rpcEndpoint: 'http://143.244.147.126:26657'
-  //   });
-
-  //   await client.init();
-
-  //   const obj = await client.swapExactAmountIn({
-  //     sender: TEST_ADDR1,
-  //     routes: [
-  //       {
-  //         poolId: '606',
-  //         tokenOutDenom:
-  //           'ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2'
-  //       }
-  //     ],
-  //     tokenIn: {
-  //       denom:
-  //         'ibc/B9E0A1A524E98BB407D3CED8720EFEFD186002F90C1B1B7964811DD0CCC12228',
-  //       amount: '4091500000'
-  //     },
-  //     tokenOutMinAmount: '733197'
-  //   });
-
-  //   const signed = await client.sign(obj);
-
-  //   console.log('obj transaction:', signed);
 };
 
-go();
+main();
