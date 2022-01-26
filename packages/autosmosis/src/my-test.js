2d25c5eeb6e16ccf2c93efaa48bc9bea38071c30
njerschow <nj18@nyu.edu>
2022-01-26 02:10:01 -0500

    finally verrified that the issue is sig

--- packages/autosmosis/src/my-test.js
+++ packages/autosmosis/src/my-test.js
@@ -13,8 +13,14 @@ import { osmosis } from './proto/generated/codecimpl';
 import axios from 'axios';
 import { TxRaw } from "cosmjs-types/cosmos/tx/v1beta1/tx";
 
-const mnemonic =
-  'health nest provide snow total tissue intact loyal cargo must credit wrist';
+const NET = "NOT_LOCAL"
+
+// localnet
+// const mnemonic =
+//   'health nest provide snow total tissue intact loyal cargo must credit wrist';
+
+//testnet
+const mnemonic = NET === 'LOCAL' ? 'health nest provide snow total tissue intact loyal cargo must credit wrist' : 'mammal wrestle hybrid cart choose flee transfer filter fly object swamp rookie'
 export const go = async () => {
   const wallet = await Secp256k1HdWallet.fromMnemonic(mnemonic, {
     prefix: 'osmo'
@@ -23,11 +29,11 @@ export const go = async () => {
   console.log({ accounts, wallet });
   const [{ address }] = accounts
   console.log({ address });
-  const rpcEndpoint = 'http://10.0.0.15:26657'; // 'http://143.244.147.126:26657';
+  const rpcEndpoint =  NET === 'LOCAL' ? 'http://10.0.0.15:26657': 'http://143.244.147.126:26657'; // 'http://143.244.147.126:26657';
   const registry = new Registry();
 
 
-  const lcdResponse = await axios.get(`http://10.0.0.15:1317/auth/accounts/${address}`);//`http://143.244.147.126:1317/auth/accounts/${address}`);
+  const lcdResponse = await axios.get(NET === 'LOCAL' ? `http://10.0.0.15:1317/auth/accounts/${address}`: `http://143.244.147.126:1317/auth/accounts/${address}`);//`http://143.244.147.126:1317/auth/accounts/${address}`);
   console.log({ lcdResponse: lcdResponse.data.result.value })
 
   const aminoTypes = new AminoTypes({
@@ -108,18 +114,18 @@ export const go = async () => {
   // console.log(signed);
 
   // const res = await client.signAndBroadcast(address, [msg], fee, "yo memo");
-  
 
-  const { accountNumber, sequence }  = await client.getSequence(address);
-  console.log({accountNumber, sequence});
+
+  const { accountNumber, sequence } = await client.getSequence(address);
+  console.log({ accountNumber, sequence });
   const txRaw = await client.sign(address, [msg], fee, 'mymemo', {
     'accountNumber': accountNumber,
     'sequence': sequence,
-    'chainId': 'localnet-1'
+    'chainId': NET === 'LOCAL' ? 'localnet-1' : 'osmosis-testnet-='
   });
   const txBytes = TxRaw.encode(txRaw).finish();
   console.log({ txRawSig: txRaw.signatures, txBytes })
-  const res = client.broadcastTx(txBytes, 100000, 1000);
+  const res = await client.broadcastTx(txBytes, 100000, 1000);
 
   console.log(res);
 
