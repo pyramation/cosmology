385a2e7f79e5cd23d16a7258e2fe7a41cc9c0696
Dan Lynch <pyramation@gmail.com>
2022-01-27 13:07:02 -0800

    refactor

--- packages/autosmosis/src/utils/osmo.js
+++ packages/autosmosis/src/utils/osmo.js
@@ -1,28 +1,28 @@
 import { prompt, promptOsmoWallet } from '../utils';
 import { promptChain  } from '../utils';
 import { getClient } from '../messages';
+import { OsmosisApiClient } from '../clients/osmosis';
+
+const osmosTestnetRests = ['http://143.244.147.126:1317'].map(value => {
+  return {
+    name: `${value} (testnet)`,
+    value
+  }
+});
+const osmosTestnetRpcs = ['http://143.244.147.126:26657'].map(value => {
+  return {
+    name: `${value} (testnet)`,
+    value
+  }
+});
 
 export const osmoRpcClient = async (argv) => {
   argv.chainToken = 'OSMO';
-  const osmosTestnetRests = ['http://143.244.147.126:1317'].map(value => {
-    return {
-      name: `${value} (testnet)`,
-      value
-    }
-  });
-  const osmosTestnetRpcs = ['http://143.244.147.126:26657'].map(value => {
-    return {
-      name: `${value} (testnet)`,
-      value
-    }
-  });
 
   const chain = await promptChain(argv);
   const wallet = await promptOsmoWallet(argv);
-//   const [account] = await wallet.getAccounts();
 
   try {
-    // const rest = chain.apis.rest.map(({ address }) => address);
     const rpc = chain.apis.rpc.map(({ address }) => address).map(value => {
       return {
         name: `${value} (mainnet)`,
@@ -49,24 +49,70 @@ export const osmoRpcClient = async (argv) => {
         message: 'rpcEndpoint',
         choices: [...rpc, ...osmosTestnetRpcs]
       }
-      //   {
-      //     type: 'list',
-      //     name: 'restEndpoint',
-      //     message: 'restEndpoint',
-      //     choices: [...rest, ...osmosTestnetRests]
-      //   }
     ];
     const { rpcEndpoint, chainId } = await prompt(questions, argv);
-    // if (osmosTestnetRests.includes(restEndpoint)) {
-    //   console.log('WARN: using TESTNET');
-    // }
-
+    if (osmosTestnetRpcs.includes(rpcEndpoint)) {
+      console.log('WARN: using TESTNET');
+    }
     const client = await getClient({ rpcEndpoint: rpcEndpoint, wallet });
-    // const address = account.address;
 
     argv.chainId = chainId;
     argv.rpcEndpoint = rpcEndpoint;
-    // argv.restEndpoint = restEndpoint;
+
+    return {client, wallet};
+
+} catch (e) {
+    console.log('error ' + e);
+  }
+};
+
+export const osmoRestClient = async (argv) => {
+  argv.chainToken = 'OSMO';
+
+  const chain = await promptChain(argv);
+  const wallet = await promptOsmoWallet(argv);
+
+  try {
+    const rest = chain.apis.rest.map(({ address }) => address).map(value => {
+      return {
+        name: `${value} (mainnet)`,
+        value
+      }
+    });
+    if (process.env.CHAIN_ID) {
+        argv.chainId = process.env.CHAIN_ID;
+    }
+    if (process.env.REST_ENDPOINT) {
+        argv.restEndpoint = process.env.REST_ENDPOINT;
+    }
+    
+    const questions = [
+      {
+        type: 'list',
+        name: 'chainId',
+        message: 'chainId',
+        choices: ['localnet-1', 'osmosis-testnet-0', 'osmosis-1']
+      },
+        {
+          type: 'list',
+          name: 'restEndpoint',
+          message: 'restEndpoint',
+          choices: [...rest, ...osmosTestnetRests]
+        }
+    ];
+    const { restEndpoint, chainId } = await prompt(questions, argv);
+    if (osmosTestnetRests.includes(restEndpoint)) {
+      console.log('WARN: using TESTNET');
+    }
+
+    // 'https://lcd-osmosis.keplr.app/'
+    const client = new OsmosisApiClient({
+      url: restEndpoint
+    });
+
+    argv.chainId = chainId;
+    argv.restEndpoint = restEndpoint;
+
     return {client, wallet};
 
 } catch (e) {
