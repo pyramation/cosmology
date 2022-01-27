b3c977ec591d0462b4e5bde24b376f45022d3c73
Dan Lynch <pyramation@gmail.com>
2022-01-27 00:26:31 -0800

    cmds

--- packages/autosmosis/src/cmds.js
+++ packages/autosmosis/src/cmds.js
@@ -1,11 +1,17 @@
 
 import _create_mnemonic_wallet from './commands/create-mnemonic-wallet';
+import _get_price from './commands/get-price';
 import _join_pool from './commands/join-pool';
+import _list_pools from './commands/list-pools';
+import _list_prices from './commands/list-prices';
 import _lock_tokens from './commands/lock-tokens';
 import _swap_exact_amount_in from './commands/swap-exact-amount-in';
 const Commands = {};
 Commands['create-mnemonic-wallet'] = _create_mnemonic_wallet;
+Commands['get-price'] = _get_price;
 Commands['join-pool'] = _join_pool;
+Commands['list-pools'] = _list_pools;
+Commands['list-prices'] = _list_prices;
 Commands['lock-tokens'] = _lock_tokens;
 Commands['swap-exact-amount-in'] = _swap_exact_amount_in;
 
---
+++ packages/autosmosis/src/commands/list-pools.js
@@ -0,0 +1,4 @@
+import { prompt, promptMnemonic } from '../utils';
+import { assets } from '../assets';
+
+export default async (argv) => {};
