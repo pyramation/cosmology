3f94a3c4c57a127640398ace28903f0851315c91
Alexander Clark <alex@m1pro.attlocal.net>
2022-01-27 23:53:07 -0500

    [WIP] polling logic

--- packages/autosmosis/__tests__/driver.test.js
+++ packages/autosmosis/__tests__/driver.test.js
@@ -1,24 +1,25 @@
 import prices from '../__fixtures__/coingecko/api/v3/simple/price/data.json'
-import { getAllSwaps } from '../src/driver/driver'
+import { Driver } from '../src/driver/driver'
 import { Token } from '../src/model/Token'
 
-describe('testGetAllSwaps', () => {
-  it('can calculate all necessary swaps', async () => {
-    const swaps = await getAllSwaps([
+describe('testGetAllJobs', () => {
+  it('can calculate all necessary jobs', async () => {
+    let driver = new Driver()
+    const jobs = await driver.getAllJobs([
       { "type": "coin", "coin": "UST", "weight": 0.3 },
-      { "type": "pool", "pool": { "coin1": "LUNA", "coin2": "UST", "id": 562, "balance": 0.5 }, "weight": 0.3 },
-      { "type": "pool", "pool": { "coin1": "ATOM", "coin2": "STARS", "id": 611, "balance": 0.7 }, "weight": 0.4 }
+      { "type": "pool", "pool": { "id": 562, "coin1": "LUNA", "coin2": "UST", "balance": 0.5 }, "weight": 0.3 },
+      { "type": "pool", "pool": { "id": 611, "coin1": "ATOM", "coin2": "STARS", "balance": 0.7 }, "weight": 0.4 }
     ])
 
-    expect(swaps).length === 6
+    expect(jobs).length === 6
 
-    expect(swaps[0].inputCoin === "OSMO")
-    expect(swaps[0].targetCoin === "UST")
-    expect(swaps[0].amount > 0)
+    expect(jobs[0].inputCoin === "OSMO")
+    expect(jobs[0].targetCoin === "UST")
+    expect(jobs[0].amount > 0)
 
-    expect(swaps[5].inputCoin === "OSMO")
-    expect(swaps[5].targetCoin === "UST")
-    expect(swaps[5].amount > 0)
+    expect(jobs[5].inputCoin === "OSO")
+    expect(jobs[5].targetCoin === "UST")
+    expect(jobs[5].amount > 0)
     // .toEqual([
     //   { "inputCoin": "LUNA", "targetCoin": "UST", "amount": 13 },
     //   { "inputCoin": "OSMO", "targetCoin": "UST", "amount": 50 },
