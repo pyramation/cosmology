f53b42d2fae683a16f071a9259feefa20387c753
njerschow <nj18@nyu.edu>
2022-01-27 19:33:18 -0500

    WIP UI (finished allocs)

--- packages/next/pages/app.js
+++ packages/next/pages/app.js
@@ -36,15 +36,34 @@ const defaultPools = [
         rewardAlloc: 100,
         isSingle: true
     },
-]
+];
+
+let saverTimeout = null;
 
 const App = ({ pools }) => {
 
-    const [ourPools, setOurPools] = useState(defaultPools);
+    const [ourPools, setOurPools] = useState(null);
     const [showPoolAdder, setShowPoolAdder] = useState(false);
     const [queuedPools, setQueuedPools] = useState([]);
 
+    function savePoolSettings() {
+        console.log("Savinng", ourPools);
+        window.localStorage.setItem('poolsConfig', JSON.stringify(ourPools))
+    }
+
+    function resetPoolSettingsSaver() {
+        if (saverTimeout) clearTimeout(saverTimeout);
+        saverTimeout = setTimeout(() => {
+            savePoolSettings();
+            saverTimeout = null;
+        }, 30)
+    }
+
     useEffect(() => {
+        if (!ourPools) {
+            const savedPoolSettings = window.localStorage.getItem('poolsConfig');
+            setOurPools(savedPoolSettings ? JSON.parse(savedPoolSettings) : defaultPools);
+        }
         if (queuedPools.length) {
             const poolsToAdd = [];
             for (let queuedPool of queuedPools) {
@@ -61,26 +80,46 @@ const App = ({ pools }) => {
                     poolsToAdd.push(queuedPool)
                 }
             }
-            setOurPools([...ourPools, ...poolsToAdd]);
+            const newPools = [...ourPools, ...poolsToAdd];
+            setOurPools(newPools);
             setQueuedPools([]);
+            resetPoolSettingsSaver();
         }
     }, [queuedPools]);
 
     function handleRewardAllocChange(pidx, newValue) {
-        console.log("handleRewardAllocChange")
-        setOurPools(ourPools.map((p, i) => {
+        // console.log("handleRewardAllocChange")
+        setOurPools(ourPools ? ourPools.map((p, i) => {
             if (i === pidx) p.rewardAlloc = newValue
             return p;
-        }));
+        }) : null);
+        resetPoolSettingsSaver();
     }
 
     function addPool(pool) {
-        console.log("addPool", ourPools);
-        setQueuedPools([pool])
+        // console.log("addPool", ourPools);
+        setQueuedPools([pool]);
     }
 
-    console.log(ourPools)
+    function deletePool(iToDelete) {
+        setOurPools(ourPools.filter((p, pi) => pi !== iToDelete));
+        resetPoolSettingsSaver();
+    }
 
+    function triggerSwapsPreview() {
+
+    /**
+     * getAllSwaps returns a list of swaps to achieve the desired allocations
+     * sum of all weights must === 1
+     * @param {*} allocationsAndWeights is a list of desired final pools/coins and their weights
+     * @returns a list of swaps
+     */
+    export function getAllSwaps (allocationsAndWeights) {
+    }
+
+    // console.log(ourPools)
+
+    const totalAlloc = ourPools ? ourPools.reduce((total, p) => total + p.rewardAlloc, 0.0000001) : 100
     return <div>
         <div className='container maxwidth-xs' style={{ marginTop: 120, textAlign: 'center' }}>
             <div className='grid-container light-border column' style={{ borderRadius: 32, alignItems: 'stretch' }}>
@@ -90,16 +129,18 @@ const App = ({ pools }) => {
                 <div className='grid-item' style={{ textAlign: 'left' }}>
                     <p className='detail-text' style={{ paddingLeft: 8 }}>Pools</p>
                     <div className='pool-list'>
-                        {ourPools.map((pool, i) => {
+                        {ourPools && ourPools.map((pool, i) => {
+                            const rewardAllocPercent = pool.rewardAlloc / totalAlloc * 100;
+
                             return <div key={pool.nickname + pool.id} className='pool light-border'>
-                                <div className='delete' onClick={() => setOurPools(ourPools.filter((p, pi) => pi !== i))}><FontAwesomeIcon icon='times' /></div>
+                                <div className='delete' onClick={() => deletePool(i)}><FontAwesomeIcon icon='times' /></div>
                                 {pool.isSingle ? <img style={{ marginRight: 39 - 26 + 5 }} className='icon' src={pool.icon} />
                                     :
                                     <PoolPairImage images={pool.images || []} height={26} />
                                 }
                                 <div>
                                     <h5 className='main-text'>{pool.nickname}</h5>
-                                    <p className='detail-text' style={{ marginLeft: 0, opacity: '50%' }}>{pool.rewardAlloc}%</p>
+                                    <p className='detail-text' style={{ marginLeft: 0, opacity: '50%' }}>{rewardAllocPercent.toFixed(2)}%</p>
                                 </div>
                                 <div style={{ flex: 1, maxWidth: '45%', marginLeft: 'auto' }}>
                                     {/* <ReactSlider style={{ width: 200 }} disabled={true} min={0} max={100} value={pool.rewardAlloc} /> */}
@@ -121,9 +162,9 @@ const App = ({ pools }) => {
                             :
                             <button onClick={() => setShowPoolAdder(true)} className='secondary-button' style={{ marginBottom: 8, alignSelf: 'center', fontSize: 14 }}><FontAwesomeIcon icon='plus' style={{ marginRight: 8 }} />Add Pool</button>
                         }
-                        <div className='pools-alloc-summary__container'>
+                        {/* <div className='pools-alloc-summary__container'>
                             <PoolAllocSummary />
-                        </div>
+                        </div> */}
                     </div>
                 </div>
                 <div className='grid-item' style={{ display: 'flex', flex: 1 }}>
