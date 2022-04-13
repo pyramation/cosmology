957723bbf6c5f96ccf7122bdc5f217a780841196
njerschow <nj18@nyu.edu>
2022-04-13 12:42:29 -0400

    identified problem with apr list

--- packages/cosmology/src/utils/osmo/aprs.js
+++ packages/cosmology/src/utils/osmo/aprs.js
@@ -54,6 +54,7 @@ export const getPoolAprs = async ({
   const {
     lockable_durations: [d1, d7, d14]
   } = await api.getLockableDurations();
+  const result = await api.getIncentivizedPools();
   const { incentivized_pools } = await api.getIncentivizedPools();
   // const gauges = await api.getActiveGauges();
 
@@ -80,28 +81,37 @@ export const getPoolAprs = async ({
   const send = [];
 
   for (let p = 0; p < poolIds.length; p++) {
+    console.log('here-4');
+
     const gaugeInfo = incentives.find(({ pool_id }) => pool_id == poolIds[p]);
     let gauge;
     if (gaugeInfo) {
       gauge = await api.getGauge(gaugeInfo.gauge_id);
     }
+    console.log('here-3');
 
-    const [{ apr_list }] = await validator.getPoolApr(poolIds[p]);
+    const result = await validator.getPoolApr(poolIds[p]);
+    console.log(result);
+    const [{ apr_list }] = result;
+    console.log('here-2');
 
     const osmoIncentives = apr_list
       .filter((i) => new Date(i.start_date) <= new Date() && i.symbol == 'OSMO')
       .map(convertAprToApyObj)
       .map(filterProps);
+    console.log('here-1');
 
     const externalIncentives = apr_list
       .filter((i) => new Date(i.start_date) <= new Date() && i.symbol != 'OSMO')
       .map(convertAprToApyObj)
       .map(filterProps);
+    console.log('here0');
 
     const futureIncentives = apr_list
       .filter((i) => new Date(i.start_date) > new Date())
       .map(convertAprToApyObj)
       .map(filterProps);
+    console.log('here1');
 
     const totalIncentives = filterProps(
       convertAprToApyObj(
@@ -122,7 +132,7 @@ export const getPoolAprs = async ({
           )
       )
     );
-
+    console.log('here2');
     send.push({
       poolId: poolIds[p],
       osmoIncentives,
